a115df0c3fce0a220a061b92ac7d3ff32ac5a64c
YP522 <yoannpetit22@free.fr>
2025-01-28 13:31:53 +0100

    :rocket: feat(server): migrate server.js to TypeScript

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-const express = require('express');
-
-const app = express()
-
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
 app.get('/', (req, res) => {
     res.json({
         status: 200,
         message: "LFM BackEnd - Update Test"
-    })
-})
-
-app.listen(8080, () => {
-    console.log('server running on port 8080');
-    
-})
+    });
+});
+const PORT = 8080;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
---
+++ server.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+    res.json({
+        status: 200,
+        message: "LFM BackEnd - Update Test"
+    });
+});
+
+const PORT = 8080;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
